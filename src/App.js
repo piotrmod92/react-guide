4b5cab620f9b4ed23f2b771e532fb13792e524d0
Piotr Modzelewski <piotrmod92@gmail.com>
2021-01-31 13:06:16 +0100

    Start up Burger Builder project

--- src/App.js
+++ src/App.js
@@ -1,47 +1,11 @@
 import React, { Component } from 'react';
-import Validation from "./components/Validation/Validation";
-import Char from "./components/Char/Char";
-import './App.css';
 
 class App extends Component {
-
-    state = {
-        text: 'myTxt'
-    }
-
-    textChangeHandler = event => {
-        this.setState({
-            text: event.target.value
-        });
-    }
-
-    removeCharHandler = event => {
-        const clickedElementIndex = event.target.id;
-        const newState = {...this.state};
-        
-        let newTextArray = newState.text.split('');
-        newTextArray.splice(clickedElementIndex, 1);
-        
-        this.setState({
-            text: newTextArray.join('')
-        })
-    }
-
-    render() {
-        return (
-            <div className="App">
-                <h1>React Lists and Conditionals</h1>
-                <input type="text" value={this.state.text} onChange={this.textChangeHandler}/>
-                <p>Text length: {this.state.text.length}</p>
-                <Validation length={this.state.text.length}/>
-                {this.state.text.split('').map((char, index) => {
-                    return (
-                        <Char key={index} id={index} click={this.removeCharHandler}>{char}</Char>
-                    )
-                })}
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div></div>
+    );
+  }
 }
 
 export default App;
